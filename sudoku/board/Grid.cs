9d4c27c97048342e3d29654690e64a952684fab9
idan <idanbe7@gmail.com>
2022-12-31 14:29:07 +0200

    updating grid class

--- sudoku/board/Grid.cs
+++ sudoku/board/Grid.cs
@@ -3,9 +3,61 @@ namespace sudoku.board
 {
 	public class Grid
 	{
-		public Grid()
-		{
-		}
-	}
-}
+		private Cell[,] cells; // matrix of cells
+        private int ROW_SIZE; // size of the raw
+        private int COL_SIZE; // size of the column
+        private int SUBGRID_SIZE; // size of the subgrid
 
+        /* constructor - gets a string which represents the grid and 
+         creats the grid */
+        public Grid(string stringGrid)
+		{
+            calcSizes(stringGrid);
+            cells = new Cell[ROW_SIZE,COL_SIZE];
+            stringToGrid(stringGrid);
+		}
+
+        /* calculates the sizes qualities according to the length of the 
+         given string */
+		private void calcSizes(string stringGrid)
+		{
+            ROW_SIZE = (int)Math.Sqrt(stringGrid.Length);
+            COL_SIZE = ROW_SIZE;
+            SUBGRID_SIZE = (int)Math.Sqrt(ROW_SIZE);
+        }
+
+        // convert the string into a grid
+        private void stringToGrid(string stringGrid)
+        {
+            int row = 0, col = 0;
+            for (int index = 0; index < stringGrid.Length; index++)
+            {
+                if (index % ROW_SIZE == 0 && index != 0)
+                {
+                    row++;
+                    col = 0;
+                }
+                cells[row, col].Value = stringGrid[index];
+                col++;
+            }
+        }
+
+        // getter for the row size
+        public int rowSize
+        {
+            get { return ROW_SIZE;  }
+        }
+
+        // getter for the column size
+        public int colSize
+        {
+            get { return COL_SIZE; }
+        }
+
+        // getter for the subgrid size
+        public int subgridSize
+        {
+            get { return SUBGRID_SIZE; }
+        }
+    }
+}
