19d49998d86bbd690877ec0ef19181e42ba61ca8
idan <idanbe7@gmail.com>
2023-01-10 01:47:06 +0200

    fixing writers

---
+++ sudoku/IO/ConsoleWriter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace sudoku.IO
+{
+	public class ConsoleWriter : IWriter
+	{
+        /* gets a grid object and prints it into the console*/
+		public void WriteGrid(byte[,] grid)
+		{
+            int gridSize = grid.GetLength(0);
+            int subgridSize = (int)Math.Sqrt(gridSize);
+            Console.WriteLine();
+            for (int row = 0; row < gridSize; row++)
+            {
+                for (int col = 0; col < gridSize; col++)
+                {
+                    Console.Write(" " + (char)(grid[row, col] + '0'));
+                    if ((col + 1) % subgridSize == 0) Console.Write(" ");
+                }
+                Console.WriteLine();
+                if ((row + 1) % subgridSize == 0) Console.WriteLine();
+            }
+        }
+	}
+}
