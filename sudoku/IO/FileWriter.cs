1af78a49afac57dd135305c4d13bf3aaf9f4455a
idan <idanbe7@gmail.com>
2023-01-10 01:02:28 +0200

    file writer, to print the solution of the sudoku grid into a file

---
+++ sudoku/IO/FileWriter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace sudoku.IO
+{
+	public class FileWriter : IWriter
+	{
+        /* gets a grid object and prints it into a file*/
+        public void WriteGrid(byte[,] grid)
+		{
+			string path = "/Users/idan.beren/Projects/sudoku/sudoku/solutions/result.txt";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                int gridSize = grid.GetLength(0);
+                int subgridSize = (int)Math.Sqrt(gridSize);
+                Console.WriteLine();
+                for (int row = 0; row < gridSize; row++)
+                {
+                    for (int col = 0; col < gridSize; col++)
+                    {
+                        sw.Write(" " + (char)(grid[row, col] + '0'));
+                        if ((col + 1) % subgridSize == 0) sw.Write(" ");
+                    }
+                    sw.WriteLine();
+                    if ((row + 1) % subgridSize == 0) sw.WriteLine();
+                }
+            }
+
+        }
+	}
+}
