e73a9baab1fb96452ed5438284fb10c27f5db558
idan <idanbe7@gmail.com>
2023-01-11 16:26:04 +0200

    adding a dunction to creat a dlx list from a cover matrix

--- sudoku/dancingLinks/DLX.cs
+++ sudoku/dancingLinks/DLX.cs
@@ -21,7 +21,52 @@ namespace sudoku.dancingLinks
 		{
             answer = new List<DancingNode>();
             result = new List<DancingNode>();
+            header = CreatDLXList(coverMatrix);
             SIZE = (int)Math.Pow(coverMatrix.Length / AMOUNT_OF_CONSTRAINTS, 1.0 / 5);
+        }
+        
+        /* converts a cover matrix into a quadruple chained list that will
+         represent the cover matrix */
+        private ColumnNode CreatDLXList(byte[,] coverMatrix)
+        {
+            // initializes parameters
+            int numberOfrows = coverMatrix.GetLength(0);
+            int numberOfColumns = coverMatrix.GetLength(1);
+            ColumnNode headerNode = new ColumnNode("header");
+            List<ColumnNode> columnNodes = new List<ColumnNode>();
+
+            // adds columns to the header node (one for each column in the cover matrix)
+            for (int number = 0; number < numberOfColumns; number++)
+            {
+                ColumnNode columnNode = new ColumnNode(number.ToString());
+                columnNodes.Add(columnNode);
+                headerNode = (ColumnNode)headerNode.LinkRight(columnNode); // links the header node to the column node
+            }
+            headerNode = headerNode.right.column ?? headerNode;
+
+            // iterates through the rows of the cover matrix
+            for (int row = 0; row < numberOfrows; row++)
+            {
+                DancingNode? previousNode = null;
+
+                // iterates through the cols of the cover matrix
+                for (int col = 0; col < numberOfColumns; col++)
+                {
+
+                    // creates a series of nodes which correspond to the ones in the row
+                    if (coverMatrix[row, col] == 1)
+                    {
+                        ColumnNode column = columnNodes[col];
+                        DancingNode newNode = new DancingNode(column);
+                        previousNode ??= newNode;
+                        column.top.LinkDown(newNode); // links the new node to the top of the column node
+                        previousNode = previousNode.LinkRight(newNode); // links the new node to the right of the previous node in the row
+                        column.size++;
+                    }
+                }
+            }
+            headerNode.size = numberOfColumns;
+            return headerNode;
         }
     }
 }
