d10ca07943948a7cc175f19a67b9a51de580ddea
idan <idanbe7@gmail.com>
2023-01-11 16:14:39 +0200

    class of the column nodes in the dlx list

---
+++ sudoku/dancingLinks/ColumnNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace sudoku.dancingLinks
+{
+    /* represents a column node */
+	public class ColumnNode : DancingNode
+	{
+		public int size; // amount of dancing nodes in the column
+		public string name; // serial number of the column
+
+        /* constructor - initializes the properties of the class */
+        public ColumnNode(string name) : base()
+		{
+			size = 0;
+			this.name = name;
+			column = this;
+		}
+    }
+}
