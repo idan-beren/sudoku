5c9af9f7c1859d4af21a10cf1d64c3b7b2b543d4
idan <idanbe7@gmail.com>
2023-01-11 16:19:47 +0200

    adding a function that converts the grid matrix into a cover matrix

--- sudoku/dancingLinks/Converter.cs
+++ sudoku/dancingLinks/Converter.cs
@@ -33,5 +33,61 @@ namespace sudoku.dancingLinks
 				for (int col = 0; col < GRID_SIZE; col++)
 					matrix[row, col] = (byte)(input[row * GRID_SIZE + col] - '0');
         }
+
+		/* converts a matrix into a cover matrix -
+		 fills the cover matrix (spare binary matrix) which represents the options
+		 to fill the empty values in the sudoku grid, by taking into account
+		 the constrains and the values in the matrix which represents the grid*/
+		private void MatrixToCoverMatrix()
+		{
+            // initializes parameters that will track the positions in the cover matrix.
+            int rowPosition = 0;
+			int cellConstraintPosition = 0;
+			int rowConstraintPosition = GRID_SIZE * GRID_SIZE * 2;
+			int colConstraintPosition = GRID_SIZE * GRID_SIZE;
+			int subgridConstraintPosition = GRID_SIZE * GRID_SIZE * 3;
+
+			// iterates over each row of the matrix
+			for (int row = 0; row < GRID_SIZE; row++)
+			{
+
+                // iterates over each column of the matrix
+                for (int col = 0; col < GRID_SIZE; col++)
+				{
+					int subgridPosition = (row / SUBGRID_SIZE) * SUBGRID_SIZE + col / SUBGRID_SIZE;
+
+                    // for each cell in the matrix iterates over a range of (1 - GRID_SIZE+1)
+                    for (int option = 1; option < GRID_SIZE + 1; option++)
+					{
+
+                        // if the value of the current cell equals zero or considered to be an option
+                        if (matrix[row, col] == 0 || matrix[row, col] == option)
+						{
+
+                            /* sets values in the cover matrix to one, its represents the constraints (row, column, subgrid, cell)
+                             that must be satisfied in order to fill an empty cell with a specific option */
+                            coverMatrix[rowPosition, cellConstraintPosition] = 1;
+                            coverMatrix[rowPosition, rowConstraintPosition + option - 1] = 1;
+                            coverMatrix[rowPosition, colConstraintPosition] = 1;
+                            coverMatrix[rowPosition, subgridConstraintPosition + option - 1 + (subgridPosition * GRID_SIZE)] = 1;
+                        }
+						rowPosition++;
+						colConstraintPosition++;
+					}
+					cellConstraintPosition++;
+				}
+				rowConstraintPosition += GRID_SIZE;
+                colConstraintPosition = GRID_SIZE * GRID_SIZE;
+            }
+        }
+
+        /* calls stringToMatrix() and matrixToCoverMatrix() and returns 
+         the cover matrix*/
+        public byte[,] GetCoverMatrix()
+		{
+			StringToMatrix();
+			MatrixToCoverMatrix();
+			return coverMatrix;
+		}
 	}
 }
